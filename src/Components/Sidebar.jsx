a97f247329e0f652f4ad0b75eeae1e4dc447177f
Majharulislam <majhuruislam535@gmail.com>
2025-02-21 11:13:01 +0600

    Add task and modal setup

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,44 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FiHome, FiUsers, FiSettings, FiLogOut, FiMenu, FiX, FiPlus } from "react-icons/fi";
+import { AuthContext } from "./Authentication";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    let [modalOpen, setModalOpen] = useState(false)
+    const axiosPublic = useAxiosPublic();
+    const { user } = useContext(AuthContext);
+
+    const handleAddTask = (e) => {
+        e.preventDefault();
+
+        const title = e.target.title.value;
+        const description = e.target.description.value;
+        const category = e.target.category.value;
+
+        const task = { title, description, time_stamp: new Date(), category, email: user?.email };
+
+        axiosPublic.post('/add_task', task)
+            .then(res => {
+                console.log(res);
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Successfully Added Task",
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+
+                }
+            })
+
+
+
+
+    }
 
     return (
         <div className="flex">
@@ -61,11 +97,79 @@ const Sidebar = () => {
                         </div>
                     </div>
                     <div className="w-2/5 flex justify-end">
-                         <div>
-                             <FiPlus  className="bg-primary text-5xl p-2 rounded-full text-black "/>
-                         </div>
+                        <div>
+
+                            <button onClick={() => setModalOpen(!modalOpen)} className="bg-primary flex cursor-pointer items-center justify-between px-4 py-2 rounded-full text-black ">
+                                <FiPlus className="text-lg mr-1 " /> Add Task
+                            </button>
+                        </div>
                     </div>
-                </div> 
+                </div>
+
+                {/* modal  */}
+
+                <div>
+                    <div className="flex flex-col items-center justify-center h-screen">
+
+
+
+
+                        {modalOpen && (
+                            <div className="fixed inset-0 flex items-center justify-center bg-[#0000002e]  bg-opacity-50">
+                                <div className="bg-white p-6 rounded-lg shadow-lg lg:w-2/5 md:w-2/5 sm:4/5">
+                                    <h3 className="font-bold text-lg text-center my-2">Add Task</h3>
+
+                                    <form onSubmit={handleAddTask}>
+                                        <div className="grid grid-cols-6 gap-6">
+
+                                            <div className="col-span-6 sm:col-span-3">
+                                                <label htmlFor="title" className="text-sm font-medium text-gray-900 block mb-2">Title:</label>
+                                                <input type="text" name="title" id="title" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" placeholder="post tile" required="" />
+                                            </div>
+
+
+                                            <div className="col-span-6 sm:col-span-3">
+                                                <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+                                                    Category
+                                                </label>
+
+                                                <select name="category" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg   block w-full p-2.5 ">
+                                                    <option value={''} disabled>Select Category</option>
+                                                    <option value="To-Do">To-Do</option>
+                                                    <option value="In Process">In Process</option>
+                                                    <option value="Done">Done</option>
+                                                </select>
+                                            </div>
+
+
+                                            <div className="col-span-full">
+                                                <label htmlFor="description" className="text-sm font-medium text-gray-900 block mb-2">Description:</label>
+                                                <textarea name="description" id="description" rows="6" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-4" placeholder="Description"></textarea>
+                                            </div>
+
+                                        </div>
+                                        <div className="p-6 border-t border-gray-200 rounded-b">
+                                            <button className="text-black bg-primary  cursor-pointer  font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="submit">Submit</button>
+                                        </div>
+                                    </form>
+
+
+                                    <button className="btn mt-4 bg-primary text-black px-4 py-1 cursor-pointer rounded-full" onClick={() => setModalOpen(false)}>Close</button>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+
+                </div>
+
+
+                {/* task category  */}
+
+
+
+
+
 
             </div>
 
